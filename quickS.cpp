8e3a90f77a8d46d4e44a27d772fa1070e6d9abe6
JuanSebastianCG <84165021+JuanSebastianCG@users.noreply.github.com>
2023-02-12 16:09:49 +0000

    Pending changes exported from your codespace

---
+++ quickS.cpp
@@ -0,0 +1,16 @@
+/*  hello world*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    /* hello wolrd */
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Hello World" << endl;
+    }
+
+    return 0;
+}
